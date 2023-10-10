252588fdb69e4ef9b1f36e39050bf5b3aa9b4083
codecraft26 <gaman0221@gmail.com>
2023-10-10 23:43:05 +0530

    feat(board):using canvas in jsx for canvas

---
+++ src/components/Board/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from 'react'
+
+const Board = () => {
+
+    const canvasRef=useRef(null)
+
+
+    useEffect(()=>{
+        if(!canvasRef.current) return 
+        const canvas =canvasRef.current;
+        const context=canvas.getContext('2d')
+        // when mouting 
+        canvas.width=window.innerWidth
+        canvas.height=window.innerHeight
+
+    },[])
+  return (
+    <div>
+    <canvas ref={canvasRef}></canvas>
+
+    </div>
+
+
+  )
+}
+
+export default Board
