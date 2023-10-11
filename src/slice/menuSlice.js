dc02abba54d8f7b0a6d2951236e06e572f155c28
codecraft26 <gaman0221@gmail.com>
2023-10-12 01:11:16 +0530

    feat(slice->toolbox)

--- src/slice/menuSlice.js
+++ src/slice/menuSlice.js
@@ -1,26 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { MENU_ITEMS } from "@/constants";
+import { createSlice } from '@reduxjs/toolkit'
+import { MENU_ITEMS } from '@/constants'
 
 const initialState = {
     activeMenuItem: MENU_ITEMS.PENCIL,
-    actionMenuItem:null
-
+    actionMenuItem: null
 }
 
 export const menuSlice = createSlice({
-    name: "menu",
+    name: 'menu',
     initialState,
     reducers: {
         menuItemClick: (state, action) => {
             state.activeMenuItem = action.payload
         },
-        actionItemClick:(state,action)=>{
-            state.actionMenuItem=action.payload
+        actionItemClick: (state, action) => {
+            state.actionMenuItem = action.payload
         }
     }
-
 })
 
-export const { menuItemClick ,actionItemClick} = menuSlice.actions
+export const {menuItemClick, actionItemClick} = menuSlice.actions
 
 export default menuSlice.reducer
